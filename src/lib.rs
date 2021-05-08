c91925647af63308993014c69717d8eb19102c37
yassun <yassun910@gmail.com>
2021-05-08 14:07:48 +0900

    cargo fmt

--- src/lib.rs
+++ src/lib.rs
@@ -1,20 +1,20 @@
 use std::collections::HashMap;
-use url::{Url};
+use url::Url;
 
 #[derive(Debug)]
 pub enum Error {
-	InvalidUrnErr,
-	NegativeAmountErr,
-    UrlParseError
+    InvalidUrnErr,
+    NegativeAmountErr,
+    UrlParseError,
 }
 
 #[derive(Debug)]
 pub struct URIResources {
     urn_scheme: String,
-	address: String,
-	amount: Option<f64>,
-	label: Option<String>,
-	message: Option<String>,
+    address: String,
+    amount: Option<f64>,
+    label: Option<String>,
+    message: Option<String>,
     params: Option<HashMap<String, String>>,
 }
 
@@ -27,7 +27,7 @@ impl URIResources {
         message: Option<String>,
         params: Option<HashMap<String, String>>,
     ) -> URIResources {
-        URIResources{
+        URIResources {
             urn_scheme,
             address,
             amount,
@@ -42,8 +42,8 @@ impl URIResources {
             return Err(Error::InvalidUrnErr);
         }
 
-        let mut url = format!( "{}{}", self.urn_scheme, ":" );
-        url = format!( "{}{}", url, self.address );
+        let mut url = format!("{}{}", self.urn_scheme, ":");
+        url = format!("{}{}", url, self.address);
 
         let mut map = HashMap::new();
 
@@ -70,12 +70,17 @@ impl URIResources {
 
         match Url::parse_with_params(&url, map) {
             Ok(parsed) => Ok(parsed.as_str().to_string()),
-            Err(_) => Err(Error::UrlParseError)
+            Err(_) => Err(Error::UrlParseError),
         }
     }
 
-    pub fn parse(url: String) -> Result<URIResources, Error> {
-        let uri = URIResources::new(
+    pub fn parse(uri: String) -> Result<URIResources, Error> {
+        let s: Vec<&str> = uri.split(':').collect();
+        if s[0] != "bitcoin" || s.len() != 2 {
+            return Err(Error::InvalidUrnErr);
+        }
+
+        let test = URIResources::new(
             String::from("bitcoin"),
             String::from("175tWpb8K1S7NmH4Zx6rewF9WQrcZv245W"),
             Some(100.0),
@@ -83,19 +88,23 @@ impl URIResources {
             Some(String::from("message")),
             Some(HashMap::new()),
         );
-        Ok(uri)
+
+        Ok(test)
     }
 }
 
 #[cfg(test)]
 mod tests {
-    use url::Url;
     use super::*;
+    use url::Url;
 
     #[test]
     fn test_build_uri() {
         let mut params = HashMap::new();
-        params.insert(String::from("somethingyoudontunderstan"), String::from("50"));
+        params.insert(
+            String::from("somethingyoudontunderstan"),
+            String::from("50"),
+        );
         params.insert(String::from("somethingelseyoudontget"), String::from("999"));
         let uri = URIResources::new(
             String::from("bitcoin"),
